2ffdc06cee846c2427671c32bbb00d2ddb33d452
Neil Shukla <neilpshukla221203@gmail.com>
2023-03-18 01:55:21 +0530

    home and card updated as per data provided from context

--- src/pages/client/home/Home.jsx
+++ src/pages/client/home/Home.jsx
@@ -2,40 +2,42 @@ import React from 'react'
 
 import Card from '../../../shared/Card'
 
-import DATABASE from './../../../data/MOCK';
 import logo from '../../../assets/logo.png'
 
 import './Home.css'
+import useHospital from '../../../context/HospitalContext';
 
 function Home() {
-  return (
-    <>
-        <div className="contain">
-            <div className="primary--text"> Hospitals </div>
-            <div className="cards">
-                <ul>
-                {
-                    DATABASE.map(
-                        (ele) => {
-                            return (
-                                <li key={DATABASE.id}> 
-                                    <Card
-                                        img={logo}
-                                        name={ele.name}
-                                        type={ele.type}
-                                        wait={ele.queue}
-                                        location={ele.address}
-                                    />
-                                </li>
+    const {hospital} = useHospital()
+    if(hospital)
+    return (
+        <>
+            <div className="contain">
+                <div className="primary--text"> Hospitals </div>
+                <div className="cards">
+                    <ul>
+                        {
+                            hospital.map(
+                                (ele) => {
+                                    return (
+                                        <li key={hospital._id}>
+                                            <Card
+                                                name={ele.name}
+                                                type={ele.type}
+                                                queue={ele.queue}
+                                                location={ele.address}
+                                                id ={ele.id}
+                                            />
+                                        </li>
+                                    )
+                                }
                             )
                         }
-                    )
-                }
-                </ul>
+                    </ul>
+                </div>
             </div>
-        </div>
-    </>
-  )
+        </>
+    )
 }
 
 export default Home
--- src/shared/Card.jsx
+++ src/shared/Card.jsx
@@ -1,34 +1,29 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 import logo from '../assets/logo.png'
 import './Card.css'
 
-function Card({img,name,type,wait,location,id}) {
+function Card({ img, name, type, wait, location, id }) {
   return (
-    <div className="card">
-        <div className="a1">
-            <div className="image">
-              <img src={img} alt="image_hospital" className='img' />
-              </div>
-        </div>
+    <NavLink to={`/${id}`}>
+      <div className="card">
         <div className="a2">
-            <div className="name">{name}</div>
-            <div className="type">{type}</div>
-            <div className="wait">WAITING TIME: {wait}</div>
+          <div className="name">{name}</div>
+          <div className="type">{type}</div>
+          <div className="wait">WAITING TIME: {wait}</div>
         </div>
         <div>
-            <div className="location">{location}</div>
-           <Link to='/:id'>
-                <div className="icon">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="#EB455F" className="bi bi-play-circle" viewBox="0 0 16 16" name="icon">
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z"/>
-                    </svg>
-                </div>
-           </Link>
+          <div className="location">{location}</div>
+          <div className="icon">
+            <svg xmlns="http://www.w3.org/2000/svg" width="3rem" height="3rem" fill="#EB455F" className="bi bi-play-circle" viewBox="0 0 16 16" name="icon">
+              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+              <path d="M6.271 5.055a.5.5 0 0 1 .52.038l3.5 2.5a.5.5 0 0 1 0 .814l-3.5 2.5A.5.5 0 0 1 6 10.5v-5a.5.5 0 0 1 .271-.445z" />
+            </svg>
+          </div>
         </div>
       </div>
+    </NavLink>
   )
 }
 
